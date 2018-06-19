9e093a7edfb6b77e58e3a6b30534634b6c5d28d9
keultae <keultae@korea.com>
2018-06-19 21:48:29 +0900

    chatrooms/PID/users/uid가 true인 채팅방만 검색한 후 검색한 값을 ChatModel.order로 디센딩

--- app/src/main/java/com/keultae/howltalk/model/ChatModel.java
+++ app/src/main/java/com/keultae/howltalk/model/ChatModel.java
@@ -7,6 +7,7 @@ public class ChatModel
 {
     public Map<String, Boolean> users = new HashMap<>();    // 채팅방 유저들
     public Map<String, Comment> comments = new HashMap<>(); // 채팅방의 대화 내용
+    public long order;  // Long.MAX_VALUE - 마지막 채팅 시간, 오름 차순 정렬시 가장 최근 값이 먼저 나오도록 하기 위해서
     public long timestamp; // 마지막 채팅 시간
 
     public static class Comment {
